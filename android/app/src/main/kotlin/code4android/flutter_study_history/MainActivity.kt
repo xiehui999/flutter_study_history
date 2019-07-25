435f2c370e76a8dff5eb9037b5e9f188264f7932
code4android <xiehui0902@163.com>
2019-07-25 15:36:30 +0800

    初始化Flutter项目

---
+++ android/app/src/main/kotlin/code4android/flutter_study_history/MainActivity.kt
@@ -0,0 +1,13 @@
+package code4android.flutter_study_history
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
