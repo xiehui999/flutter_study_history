0f47be7209f4de9ca2332ec585a9c12b4d7dda37
code4android <xiehui0902@163.com>
2019-07-26 18:17:49 +0800

    新增base_library

---
+++ base_library/lib/base_library.dart
@@ -0,0 +1,9 @@
+library base_library;
+
+export 'src/res/index.dart';
+
+///// A Calculator.
+//class Calculator {
+//  /// Returns [value] plus 1.
+//  int addOne(int value) => value + 1;
+//}
---
+++ base_library/lib/src/res/colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color app_main = Color(0xFF666666);
+  static const Color app_bg = Color(0xfff5f5f5f5);
+
+  static const Color transparent_80 = Color(0x80000000);
+  static const Color white_19 = Color(0X19FFFFFF);
+
+  static const Color text_dark = Color(0xFF333333);
+  static const Color text_normal = Color(0xFF666666);
+  static const Color text_gray = Color(0xFF999999);
+
+  static const Color divider = Color(0xffe5e5e5);
+
+  static const Color gray_33 = Color(0xFF333333);
+  static const Color gray_66 = Color(0xFF666666);
+  static const Color gray_99 = Color(0xFF999999);
+  static const Color common_orange = Color(0XFFFC9153);
+  static const Color gray_ef = Color(0XFFEFEFEF);
+
+  static const Color gray_f0 = Color(0xfff0f0f0);
+  static const Color gray_f5 = Color(0xfff5f5f5);
+  static const Color gray_cc = Color(0xffcccccc);
+  static const Color gray_ce = Color(0xffcecece);
+  static const Color green_1 = Color(0xff009688);
+  static const Color green_62 = Color(0xff626262);
+  static const Color green_e5 = Color(0xffe5e5e5);
+
+  static const Color green_de = Color(0xffdedede);
+}
