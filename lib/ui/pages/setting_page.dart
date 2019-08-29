62f3ff9d620fe74fa919b8a06c5999e80fcbf026
code4android <xiehui0902@163.com>
2019-08-29 18:04:32 +0800

    设置页面

--- lib/ui/pages/setting_page.dart
+++ lib/ui/pages/setting_page.dart
@@ -12,6 +12,70 @@ class SettingPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(IntlUtil.getString(context, Ids.titleSetting)),
       ),
+      body: ListView(
+        children: <Widget>[
+          new ExpansionTile(
+            title: new Row(
+              children: <Widget>[
+                Icon(
+                  Icons.color_lens,
+                  color: AppColors.gray_66,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(IntlUtil.getString(context, Ids.titleTheme)),
+                ),
+              ],
+            ),
+            children: <Widget>[
+              new Wrap(
+                children: themeColorMap.keys.map((String key) {
+                  Color color = themeColorMap[key];
+                  return new InkWell(
+                    onTap: () {},
+                    child: new Container(
+                      margin: EdgeInsets.all(5),
+                      width: 36,
+                      height: 36,
+                      color: color,
+                    ),
+                  );
+                }).toList(),
+              )
+            ],
+          ),
+          new ListTile(
+            title: new Row(
+              children: <Widget>[
+                Icon(
+                  Icons.language,
+                  color: AppColors.gray_66,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(IntlUtil.getString(context, Ids.titleLanguage)),
+                )
+              ],
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                    languageModel == null
+                        ? IntlUtil.getString(context, Ids.languageAuto)
+                        : IntlUtil.getString(context, languageModel.titleId,
+                            languageCode: 'zh', countryCode: 'CH'),
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: AppColors.gray_99,
+                    )),
+                Icon(Icons.keyboard_arrow_right)
+              ],
+            ),
+            onTap: () {},
+          )
+        ],
+      ),
     );
   }
 }
